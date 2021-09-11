cc9515a6dd451fa04a02c80fadf2d8b7ce4e6b74
Davis W. Frank <dwfrank@gmail.com>
2021-09-11 09:54:52 -0700

    firsties

---
+++ lib/solar_payoff.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "solar_payoff/version"
+
+module SolarPayoff
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/solar_payoff_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SolarPayoff do
+  it "has a version number" do
+    expect(SolarPayoff::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
